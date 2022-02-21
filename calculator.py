ffd7f0430577b21ba842899b984d2f4d39905d47
Krzysztof Sawicki <krzysiek.saw@gmail.com>
2022-02-21 18:16:53 +0100

    Functioning program without API integration

--- calculator.py
+++ calculator.py
@@ -7,18 +7,22 @@ def dividend_amount():
 
     total_div_wo_tax = total_div * (1-float(e11_value.get())*0.01)
 
-    print("Total amount of dividend: " + str(total_div))
-    print("Total amount of dividend without tax: " + str(total_div_wo_tax))
-    print("Total amount of dividend per year: " + str(total_div*div_freq))
-    print("Total amount of dividend per year without tax: " + str(total_div_wo_tax*div_freq))
+    total_div_per_y = total_div * float(e12_value.get())
 
+    total_div_per_y_wo_tax = total_div_per_y * (1-float(e11_value.get())*0.01)
 
-shares_amount = 20.6
-div_per_share = 0.22
-tax = 19
-div_freq = 4
+    t1.delete("1.0", END)
+    t1.insert(END,total_div)
 
-dividend_amount(shares_amount, div_per_share, tax, div_freq)
+    t2.delete("1.0", END)
+    t2.insert(END,total_div_wo_tax)
+
+    t3.delete("1.0", END)
+    t3.insert(END,total_div_per_y)
+
+    t4.delete("1.0", END)
+    t4.insert(END,total_div_per_y_wo_tax)
+
 
 # Create the Label widgets
 e1 = Label(window, text = "Enter the amount oh shares")
@@ -46,7 +50,32 @@ t2 = Text(window, height = 1, width = 20)
 t3 = Text(window, height = 1, width = 20)
 t4 = Text(window, height = 1, width = 20)
 
-t5 = Text(window, height = 1, width = 20)
-t6 = Text(window, height = 1, width = 20)
-t7 = Text(window, height = 1, width = 20)
-t8 = Text(window, height = 1, width = 20)
+# Create the Button Widget
+b1 = Button(window, text = "Calculate", command = dividend_amount)
+
+# grid method is used for placing
+# the widgets at respective positions
+# in table like structure
+e1.grid(row = 0, column = 0)
+e2.grid(row = 1, column = 0)
+e3.grid(row = 2, column = 0)
+e4.grid(row = 3, column = 0)
+e5.grid(row = 5, column = 0)
+e6.grid(row = 6, column = 0)
+e7.grid(row = 7, column = 0)
+e8.grid(row = 8, column = 0)
+
+e9.grid(row = 0, column = 2)
+e10.grid(row = 1, column = 2)
+e11.grid(row = 2, column = 2)
+e12.grid(row = 3, column = 2)
+
+b1.grid(row = 4, column = 0)
+
+t1.grid(row = 5, column = 2)
+t2.grid(row = 6, column = 2)
+t3.grid(row = 7, column = 2)
+t4.grid(row = 8, column = 2)
+
+# Start the GUI
+window.mainloop()
